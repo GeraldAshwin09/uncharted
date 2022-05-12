3787e4e3abb0c25491ebc0c27e6dfa9ab1bc46ff
GeraldAshwin09 <89208455+GeraldAshwin09@users.noreply.github.com>
2022-05-12 15:25:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,356 @@
+
+
+function signup()
+{
+    var user=document.getElementById("user").value;
+localStorage.setItem("user1",user);
+window.location="mg.html";
+}
+
+function move1()
+{
+    window.location="animate.html";
+}
+
+function move2()
+{
+    window.location="adv.html";
+}
+
+function move3()
+{
+    window.location="sci.html";
+}
+
+
+function booked1()
+{
+   document.getElementById("a").style.color="green";
+}
+
+function booked2()
+{
+   document.getElementById("b").style.color="green";
+}
+
+function booked3()
+{
+   document.getElementById("c").style.color="green";
+}
+
+function booked4()
+{
+   document.getElementById("d").style.color="green";
+}
+
+function booked5()
+{
+   document.getElementById("e").style.color="green";
+}
+
+function booked6()
+{
+   document.getElementById("f").style.color="green";
+}
+
+function booked7()
+{
+   document.getElementById("g").style.color="green";
+}
+
+
+
+function booked8()
+{
+   document.getElementById("h").style.color="green";
+}
+
+
+function booked9()
+{
+   document.getElementById("i").style.color="green";
+}
+
+
+function booked10()
+{
+   document.getElementById("j").style.color="green";
+}
+
+
+function booked11()
+{
+   document.getElementById("k").style.color="green";
+}
+
+
+function booked12()
+{
+   document.getElementById("l").style.color="green";
+}
+
+
+function booked13()
+{
+   document.getElementById("m").style.color="green";
+}
+
+
+function booked14()
+{
+   document.getElementById("n").style.color="green";
+}
+
+
+function booked15()
+{
+   document.getElementById("o").style.color="green";
+}
+
+
+function booked16()
+{
+   document.getElementById("p").style.color="green";
+}
+
+
+function booked17()
+{
+   document.getElementById("q").style.color="green";
+}
+
+
+function booked18()
+{
+   document.getElementById("r").style.color="green";
+}
+
+
+function booked19()
+{
+   document.getElementById("s").style.color="green";
+}
+
+
+
+function booked20()
+{
+   document.getElementById("t").style.color="green";
+}
+
+
+
+
+function booked21()
+{
+   document.getElementById("u").style.color="green";
+}
+
+
+
+function booked22()
+{
+   document.getElementById("v").style.color="green";
+}
+
+
+function booked23()
+{
+   document.getElementById("w").style.color="green";
+}
+
+
+function booked24()
+{
+   document.getElementById("x").style.color="green";
+}
+
+
+function booked25()
+{
+   document.getElementById("y").style.color="green";
+}
+
+
+function booked26()
+{
+   document.getElementById("z").style.color="green";
+}
+
+
+
+function booked27()
+{
+   document.getElementById("1").style.color="green";
+}
+
+
+
+
+function booked28()
+{
+   document.getElementById("2").style.color="green";
+}
+
+
+function booked29()
+{
+   document.getElementById("3").style.color="green";
+}
+
+
+function booked30()
+{
+   document.getElementById("4").style.color="green";
+}
+
+
+function booked31()
+{
+   document.getElementById("5").style.color="green";
+}
+
+
+function booked32()
+{
+   document.getElementById("6").style.color="green";
+}
+
+
+function booked33()
+{
+   document.getElementById("7").style.color="green";
+}
+
+function booked34()
+{
+   document.getElementById("8").style.color="green";
+}
+
+
+
+function booked35()
+{
+   document.getElementById("9").style.color="green";
+}
+
+
+function booked36()
+{
+   document.getElementById("10").style.color="green";
+}
+
+
+
+function booked37()
+{
+   document.getElementById("11").style.color="green";
+}
+
+
+function booked38()
+{
+   document.getElementById("12").style.color="green";
+}
+
+
+function booked39()
+{
+   document.getElementById("13").style.color="green";
+}
+
+
+function booked40()
+{
+   document.getElementById("14").style.color="green";
+}
+
+
+function booked41()
+{
+   document.getElementById("15").style.color="green";
+}
+
+
+
+function booked42()
+{
+   document.getElementById("16").style.color="green";
+}
+
+
+function booked43()
+{
+   document.getElementById("17").style.color="green";
+}
+
+
+function booked44()
+{
+   document.getElementById("18").style.color="green";
+}
+
+function booked45()
+{
+   document.getElementById("19").style.color="green";
+}
+
+function booked46()
+{
+   document.getElementById("20").style.color="green";
+}
+
+function booked47()
+{
+   document.getElementById("21").style.color="green";
+}
+
+function booked48()
+{
+   document.getElementById("22").style.color="green";
+}
+
+
+function booked49()
+{
+   document.getElementById("23").style.color="green";
+}
+
+function booked50()
+{
+   document.getElementById("24").style.color="green";
+}
+
+
+
+function change()
+{
+   document.getElementById("present").src="TO.jpeg";
+window.location="book.html";
+
+}
+
+function disp2()
+{
+window.location="book.html"
+}
+
+function disp3()
+{
+window.location="book.html"
+}
+
+function disp4()
+{
+window.location="book.html"
+}
+
+function disp5()
+{
+window.location="book.html"
+}
+
+function disp6()
+{
+window.location="book.html"
+}
